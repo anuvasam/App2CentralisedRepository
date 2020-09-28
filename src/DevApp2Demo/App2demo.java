d6322d07869d452705356a6edc60e8887b5606d7
Vinay-PC <anuvasam@gmail.com>
2020-09-28 17:38:46 +0530

    committed the second string

--- src/DevApp2Demo/App2demo.java
+++ src/DevApp2Demo/App2demo.java
@@ -1,5 +1,10 @@
 package DevApp2Demo;
 
 public class App2demo {
+	public static void main(String[] args) {
+	
+		System.out.println("First demoMethod");
+		System.out.println("First demoMethod2");		
+	}
 
 }

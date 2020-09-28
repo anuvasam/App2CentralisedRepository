785e0238040a9f3cd9812eb75d72888154226a5f
Vinay-PC <anuvasam@gmail.com>
2020-09-28 17:40:52 +0530

    adding the second file

---
+++ src/DevApp2Demo/App2demo2.java
@@ -0,0 +1,10 @@
+package DevApp2Demo;
+
+import javax.jws.soap.SOAPBinding;
+
+public class App2demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Secondfile shared");
+	}
+}
